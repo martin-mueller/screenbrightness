380676b454ce91e55dfc62a91603a79a45da64ba
martin-mueller <martin_mu@gmx.de>
2014-04-06 22:29:15 +0200

    totally uncluttered edition

--- applet.js
+++ applet.js
@@ -1,11 +1,24 @@
-/*TODO: i2c-tools i2cdetect*/
-/* install-script: add i2c-dev to modules */
-/* "usermod -aG i2c <current-user>" (give user access to /dev/i2c) */
+/**
+ * TODO: i2c-tools i2cdetect
+ *  read /sys/bus/i2c/devices/
+ * install-script: add i2c-dev to modules 
+ * "usermod -aG i2c <current-user>" (give user access to /dev/i2c) 
+ *
+ * TODO: add keyboard hotkeys 
+ * 
+ */
 const Applet = imports.ui.applet;
 const GLib = imports.gi.GLib;
+const Gio  = imports.gi.Gio;
 const PopupMenu = imports.ui.popupMenu;
 const Lang = imports.lang;
 const Util = imports.misc.util;
+const FileUtils = imports.misc.fileUtils;
+const Settings = imports.ui.settings;
+
+
+let i2c_path = '/sys/bus/i2c/devices/';
+const i2c_dir = Gio.file_new_for_path(i2c_path);
 
 function MyApplet(orientation) {
     this._init(orientation);
@@ -19,7 +32,24 @@ MyApplet.prototype = {
         Applet.IconApplet.prototype._init.call(this, orientation);
  
         try {
-             
+            /* Look for ddcc-tool (old) or ddccontrol (newer) installed, else use xrandr (sets Brightness software-based) */
+            this._command = this._probeCmdTools();
+            //this._findDevices();
+            /*Settings-Api look here: http://segfault.linuxmint.com/2013/05/applet-desklet-extension-settings-api-a-brief-example/ */
+            this.settings = new Settings.AppletSettings(this, "screenbrightness@spezo"); 
+            this.settings.bindProperty(Settings.BindingDirection.IN,  // Setting type
+                                 "i2c-dev",             // The setting key
+                                 "i2c-dev",             // The property to manage (this.i2c-dev)
+                                 this.dev_changed,  // Callback when value changes
+                                 null);               // Optional callback data
+
+            this.settings.bindProperty(Settings.BindingDirection.IN,
+                                 "keybinding-test",
+                                 "keybinding",
+                                 this.on_keybinding_changed,
+                                 null);
+
+
             this.set_applet_icon_name("display-brightness-symbolic");
             this.set_applet_tooltip("Brightness");
 
@@ -45,15 +75,30 @@ MyApplet.prototype = {
             this._cmd = 'ddcci-tool -r 0x10 /dev/i2c-1';
             let ret = GLib.spawn_command_line_sync(this._cmd);
             this._brightness = this._findBrightnessFromOutput(ret[1].toString());
-            // global.logError('wert ' + this._brightness);
+            //global.logError('wert ' + this._brightness);
             // debug(this._brightness.toString()):
             this._brightnessSlider.setValue(this._brightness/100);
+             this.textItem.label.text = "Brightness " + this._brightness.toString();
+           /* Let's set up our applet's initial state now that we have our setting properties defined */
+            this.on_keybinding_changed();
         }
         catch (e) {
             global.logError(e);
         }
      },
- 
+    
+
+     _probeCmdTools: function(){
+        let out = GLib.spawn_command_line_sync('which ddcci-tool');
+        global.logError(out[1]);
+        return '';
+     },
+
+
+    dev_changed: function(){
+        //this.actor.width = this.width;
+    },
+
     on_applet_clicked: function(event) {
       this.menu.toggle();      
     },
@@ -74,7 +119,32 @@ MyApplet.prototype = {
                 return b;
             }
          }
-    }
+    },
+
+
+/* i2c-dev Stuff */
+    _findDevices: function () {
+         FileUtils.listDirAsync(i2c_dir, Lang.bind(this, this._addDevicesFomDir));
+    },
+    _addDevicesFomDir: function(dir_entry){
+        let pattern = /^i2c-[0-9]+/;
+        for (let i in dir_entry)
+            if (pattern.test(dir_entry[i].get_name())) global.log(dir_entry[i].get_name());
+    },
+
+/* hotkey stuff */
+   on_keybinding_changed: function() {
+       Main.keybindingManager.addHotKey("must-be-unique-id", this.keybinding, Lang.bind(this, this.on_hotkey_triggered));
+    },
+
+  on_hotkey_triggered: function() {
+      //  this.set_applet_label(_("YOU USED THE HOTKEY!!!"));
+
+        // let timeoutId = Mainloop.timeout_add(3000, Lang.bind(this, function() {
+        //     this.on_settings_changed();
+        // }));
+    },
+
 
 };
  
--- readme.md
+++ readme.md
@@ -3,7 +3,11 @@
 I wrote this to control the brightness of an external lcd - monitor from my notebook.
 It is just a wrapper to control __ddcci-tool__ from the taskbar.
 Basically, it does:
+
 ````ddcci-tool -r 0x10 -w brightness_in_% /dev/i2c-1````
+
+OR 
+````xrandr --output VGA1 --brightness [0-1]````
 
 You should change "/dev/i2c-1" to your device.
 
@@ -18,3 +22,17 @@ _needs ddci-tool <http://jaffar.cs.msu.su/oleg/ddcci/>_
 or add i2c-dev to /etc/modules to make it permanent
 * give your user rw - access to the i2c -dev by adding them to the group i2c:
 	````usermod -Ga i2c YourUsername````
+
+### installing ddccontrol
+* make: add flag -lm
+ <http://stackoverflow.com/questions/1033898/why-do-you-have-to-link-the-math-library-in-c> 
+set LD_LIBRARY_PATH : ```` export LD_LIBRARY_PATH="/usr/local/lib````
+
+### Monitor detection 
+
+### Useful local files
+~/.config/monitors.xml
+
+### links
+* <https://github.com/lordamit/Brightness/> : Gnome Brightness Controller (Python), requires Package python-pyside, 
+  <https://dl.dropboxusercontent.com/u/84627545/brightness_1.0.1_all.deb>
